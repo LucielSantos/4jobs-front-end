38478de0d188841a747dcd1679f5c0f6cadfeba5
Luciel Santos <luciel.santos@ubistart.com>
2021-04-28 21:07:56 -0300

    remove warning

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import { LoginViewProps } from '.';
 import { Typography, Input, Form, Button } from '../../components';
 import { loginValidationSchema } from '../../validationSchemas';
