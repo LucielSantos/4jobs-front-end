e529566f1f2ab7bc94597a7937293ab5626a6434
Luciel Santos <luciel.santos@ubistart.com>
2021-04-29 23:47:26 -0300

    redirect page scheam

--- src/pages/Main/main.tsx
+++ src/pages/Main/main.tsx
@@ -1,8 +1,10 @@
 import React, { useCallback, useLayoutEffect } from 'react';
 import { MainViewProps } from '.';
+import { userType } from '../../constants';
 import { navbarStateByRoute, routePaths } from '../../routes';
 
 import { Router } from '../../routes/components';
+import { isAuthenticated } from '../../utils';
 import { Navbar, Notifier } from './components';
 
 import { Container } from './styles';
@@ -24,6 +26,10 @@ export const MainView: React.FC<MainViewProps> = ({
   );
 
   const redirectPage = useCallback(() => {
+    // TODO: implements redirect candidate
+
+    if (isAuthenticated(userType.company)) history.push(routePaths.COMPANY_JOBS);
+
     if (history.location.pathname === '/') history.push(routePaths.LOGIN);
   }, [history]);
 
@@ -34,6 +40,11 @@ export const MainView: React.FC<MainViewProps> = ({
 
     history.listen(location => {
       switchNavbarState(location.pathname);
+    });
+
+    history.listen(location => {
+      if (location.pathname === '/' || location.pathname === routePaths.LOGIN)
+        redirectPage();
     });
     // eslint-disable-next-line
   }, []);
