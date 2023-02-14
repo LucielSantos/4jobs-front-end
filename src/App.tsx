0de72c9549b234c583a9bd7bd63941304455e02c
LucielSantos <luciel.vitor.bs@gmail.com>
2023-02-14 20:50:51 -0300

    some modifications on eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { Router, Route, Switch } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 import { ThemeProvider as MUIThemeProvider } from '@material-ui/core';
 import { SnackbarProvider } from 'notistack';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Route, Router, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
-import store from './store';
 import { routePaths } from './routes';
-import theme from './styles/theme';
+import store from './store';
 import muiTheme from './styles/muiTheme';
+import theme from './styles/theme';
 
-import { globalStye as GlobalStyle } from './styles/global';
 import Main from './pages/Main';
+import { globalStye as GlobalStyle } from './styles/global';
 import { history } from './utils';
 
 const App: React.FC = () => {
