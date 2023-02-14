41e136429cafe358174a9a66fc9815b55314a473
codefactor-io <support@codefactor.io>
2023-02-14 19:01:49 +0000

    [CodeFactor] Apply fixes

--- src/components/flex/flex.ts
+++ src/components/flex/flex.ts
@@ -1,29 +1,29 @@
-import styled, { DefaultTheme } from 'styled-components';
+import styled, { DefaultTheme } from "styled-components";
 
 type TFlexAlignItems =
-  | 'normal'
-  | 'center'
-  | 'flex-start'
-  | 'flex-end'
-  | 'stretch'
-  | 'baseline';
+  | "normal"
+  | "center"
+  | "flex-start"
+  | "flex-end"
+  | "stretch"
+  | "baseline";
 
 type TFlexJustifyContent =
-  | 'normal'
-  | 'center'
-  | 'flex-start'
-  | 'flex-end'
-  | 'space-between'
-  | 'space-around'
-  | 'space-evenly';
+  | "normal"
+  | "center"
+  | "flex-start"
+  | "flex-end"
+  | "space-between"
+  | "space-around"
+  | "space-evenly";
 
-type TFlexFlexDirection = 'row' | 'column';
+type TFlexFlexDirection = "row" | "column";
 
-type TFlexFlexWrap = 'wrap' | 'nowrap';
+type TFlexFlexWrap = "wrap" | "nowrap";
 
-type TMargin = keyof DefaultTheme['spacings'];
+type TMargin = keyof DefaultTheme["spacings"];
 
-type TBackgroundColor = keyof DefaultTheme['colors'];
+type TBackgroundColor = keyof DefaultTheme["colors"];
 
 interface IFlex {
   alignItems?: TFlexAlignItems;
@@ -45,23 +45,24 @@ interface IFlex {
 export const Flex = styled.div<IFlex>`
   display: flex;
 
-  width: ${({ notFullWidth = false }) => (notFullWidth ? 'auto' : '100%')};
+  width: ${({ notFullWidth = false }) => (notFullWidth ? "auto" : "100%")};
 
-  flex-direction: ${({ flexDirection = 'row' }) => flexDirection};
-  align-items: ${({ alignItems = 'normal' }) => alignItems};
-  justify-content: ${({ justifyItems = 'normal' }) => justifyItems};
-  flex-wrap: ${({ flexWrap = 'nowrap' }) => flexWrap};
+  flex-direction: ${({ flexDirection = "row" }) => flexDirection};
+  align-items: ${({ alignItems = "normal" }) => alignItems};
+  justify-content: ${({ justifyItems = "normal" }) => justifyItems};
+  flex-wrap: ${({ flexWrap = "nowrap" }) => flexWrap};
 
   background-color: ${({ theme, backgroundColor }) =>
-    backgroundColor ? theme.colors[backgroundColor] : 'transparent'};
-  flex-wrap: ${({ flexWrap = 'nowrap' }) => flexWrap};
+    backgroundColor ? theme.colors[backgroundColor] : "transparent"};
+  flex-wrap: ${({ flexWrap = "nowrap" }) => flexWrap};
 
   border-radius: ${({ borderRadius }) => borderRadius || 0};
 
   padding: ${({ padding }) => padding || 0};
   box-sizing: border-box;
 
-  margin-top: ${({ theme, marginTop }) => (marginTop ? theme.spacings[marginTop] : 0)};
+  margin-top: ${({ theme, marginTop }) =>
+    marginTop ? theme.spacings[marginTop] : 0};
   margin-right: ${({ theme, marginRight }) =>
     marginRight ? theme.spacings[marginRight] : 0};
   margin-bottom: ${({ theme, marginBottom }) =>
